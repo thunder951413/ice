46a12384fdcf3c22f75ae85db86a40b0f9b2afe3
Yan Yan <thunder951413@gmail.com>
2026-09-19 16:50:45 +0800

    Remove empty settings toolbar item on macOS 27

--- Ice/UI/ViewModifiers/RemoveSidebarToggle.swift
+++ Ice/UI/ViewModifiers/RemoveSidebarToggle.swift
@@ -9,8 +9,5 @@ extension View {
     /// Removes the sidebar toggle button from the toolbar.
     func removeSidebarToggle() -> some View {
         toolbar(removing: .sidebarToggle)
-            .toolbar {
-                Color.clear
-            }
     }
 }
